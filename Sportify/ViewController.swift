f4dc867ab3b86f113c180a13291b64ba18aa1493
ramezHamdiSa'eed <ramez.hamdi4work@gmail.com>
2024-04-23 13:24:45 -0700

    update icon

--- Sportify/ViewController.swift
+++ Sportify/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SwiftyOnboard
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SwiftyOnboardDataSource {
+//    var visitedPages = -1;
+//    let onBoardingPages : [OnBoardingPage] = [OnBoardingPage(title: "first Page", subtitle: "lorem ipsum"), OnBoardingPage(title: "second Page", subtitle: "lorem ipsum")]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let swiftyOnboard = SwiftyOnboard(frame: view.frame)
+        view.addSubview(swiftyOnboard)
+        swiftyOnboard.dataSource = self
     }
 
+    func swiftyOnboardNumberOfPages(_ swiftyOnboard: SwiftyOnboard) -> Int {
+               return 3
+           }
+
+    func swiftyOnboardPageForIndex(_ swiftyOnboard: SwiftyOnboard, index: Int) -> SwiftyOnboardPage? {
+               let page = SwiftyOnboardPage()
+//        visitedPages += 1
+//        page.title.text = onBoardingPages[index].title
+//        page.subTitle.text = onBoardingPages[index].subtitle
+               return page
+           }
 
 }
 
