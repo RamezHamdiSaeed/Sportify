be6b18f9051b5f69ae40ad26523dadccbbe10865
HadirElnagdy <87231099+HadirElnagdy@users.noreply.github.com>
2024-04-30 18:01:03 +0300

    Testing Network

---
+++ SportifyTests/MockingTests/MockingNetworkTest.swift
@@ -0,0 +1,22 @@
+//
+//  MockingNetworkTest.swift
+//  SportifyTests
+//
+//  Created by Hadir on 30/04/2024.
+//
+
+import XCTest
+@testable import Sportify
+
+final class MockingNetworkTest: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+  
+
+}
--- SportifyTests/SportifyTests.swift
+++ SportifyTests/SportifyTests.swift
@@ -18,19 +18,38 @@ final class SportifyTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testGetLeagues(){
+        let expectation = expectation(description: "Waiting for api")
+        
+        RemoteDataSourceImpl.getLeagues(of: .football){
+            result in
+            switch result {
+            case .success(let success):
+                XCTAssertNotNil(success, "Response is nil")
+            case .failure(let failure):
+                XCTFail()
+            }
+            expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 10)
+    }
+    
+    func testGetEvents(){
+        let expectation = expectation(description: "Waiting for api")
+        
+        RemoteDataSourceImpl.getEvents(of: .football, from: "2021-12-01", to: "2024-04-01", for: "177"){
+            result in
+            switch result {
+            case .success(let success):
+                XCTAssertNotNil(success, "Response is nil")
+            case .failure(let failure):
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10)
     }
 
 }
